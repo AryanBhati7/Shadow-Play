ebb9e694ba3a70ba7c4588f0e1135a599c9a25cc
Aryan Bhati <aryanbhati9638@gmail.com>
2024-06-04 18:14:39 +0530

    minor bugs solved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,6 @@ const registerUser = asyncHandler(async (req, res) => {
   //return res
 
   const { fullName, email, username, password } = req.body;
-  console.log("email :", email);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -23,22 +22,29 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
   if (existedUser) throw new ApiError(409, "Email or Username already exists");
 
-  console.log(req.files);
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-  if (avatarLocalPath) throw new ApiError(400, "Avatar is required");
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar is required");
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-  if (avatar) throw new ApiError(400, "Avatar uploading failed");
+  if (!avatar) throw new ApiError(400, "Avatar uploading failed");
 
   const user = await User.create({
     fullName,
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 
 const port = process.env.PORT || 8000;
