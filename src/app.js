a37d58ea6b529c1060a239e208d0ee8c36e07dec
Aryan Bhati <aryanbhati9638@gmail.com>
2024-06-09 19:58:53 +0530

    feat: Add video router and routes

--- src/app.js
+++ src/app.js
@@ -18,8 +18,10 @@ app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 //routes declaration
-app.use("/api/v1/users", userRouter);
 // http://localhost:8000/api/v1/users/register
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 export { app };
