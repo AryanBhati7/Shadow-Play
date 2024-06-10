d6855c3b5dd4b82291e222cd79b252d2f0fe7a56
Aryan Bhati <aryanbhati9638@gmail.com>
2024-06-10 22:51:21 +0530

    feat: Add tweet like comment routes

--- src/app.js
+++ src/app.js
@@ -21,12 +21,14 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 //routes declaration
 // http://localhost:8000/api/v1/users/register
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/videos", videoRouter);
-app.use("/api/v1", likeRouter);
-app.use("/api/v1", commentRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/tweet", tweetRouter);
 
 export { app };
