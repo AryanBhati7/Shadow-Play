69ff23cd667785611e1c3534fdbb633cfd56a536
Aryan Bhati <aryanbhati9638@gmail.com>
2024-06-11 19:11:13 +0530

    feat: Fetch channel stats including total subscribers, likes, views, and videos

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,11 +7,81 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const userId = req.user._id;
+
+  const totalSubscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        subscribersCount: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+  if (!totalSubscribers)
+    throw new ApiError(501, "Failed to fetch Total subscribers");
+
+  const videoStats = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $project: {
+        totalLikes: {
+          $size: "$likes",
+        },
+        totalViews: "$views",
+        totalVideos: 1,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalLikes: {
+          $sum: "$totalLikes",
+        },
+        totalViews: {
+          $sum: "$totalViews",
+        },
+        totalVideos: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+  if (!videoStats) throw new ApiError(501, "Failed to fetch Video Stats");
+
+  const channelStats = {
+    totalSubscribers: totalSubscribers[0]?.subscribersCount || 0,
+    totalLikes: videoStats[0]?.totalLikes || 0,
+    totalViews: videoStats[0]?.totalViews || 0,
+    totalVideos: videoStats[0]?.totalVideos || 0,
+  };
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channelStats, "Channel Stats fetched successfully")
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-  // TODO: Get all the videos uploaded by the channel
   const userId = req.user._id;
 
   const channelVideos = await Video.aggregate([
