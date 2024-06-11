87f3b734c980a9eb64f87748e8f9273201269d92
Aryan Bhati <aryanbhati9638@gmail.com>
2024-06-11 14:06:18 +0530

    feat: Add dashboard router and routes

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import videoRouter from "./routes/video.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
 // http://localhost:8000/api/v1/users/register
@@ -30,5 +31,6 @@ app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/like", likeRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -12,6 +12,60 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+  const userId = req.user._id;
+
+  const channelVideos = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        "video.url": 1,
+        "thumbnail.url": 1,
+        title: 1,
+        description: 1,
+        // dateUploaded: {
+        //   year: 1,
+        //   month: 1,
+        //   day: 1,
+        // },
+        isPublished: 1,
+        likesCount: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+
+  if (!channelVideos) throw new ApiError(501, "Failed to fetch Channel Videos");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channelVideos, "Channel Videos fetched Successfully")
+    );
 });
 
 export { getChannelStats, getChannelVideos };
