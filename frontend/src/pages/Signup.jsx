2df9484279b27906b4998b5697a25a7dcc53e648
Aryan Bhati <aryanbhati9638@gmail.com>
2024-06-14 07:28:01 +0530

    signup image uploader done

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Logo, Input, SpButton } from "../components/index.js";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { MdOutlineCloudUpload } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 
 function Signup() {
   const schema = z.object({
@@ -38,6 +40,8 @@ function Signup() {
     register,
     handleSubmit,
     setError,
+    Controller,
+    control,
     formState: { errors, isSubmitting },
   } = useForm({
     resolver: zodResolver(schema),
@@ -46,6 +50,36 @@ function Signup() {
   const createAccount = (data) => {
     console.log(data);
   };
+  const [profilePic, setProfilePic] = useState(null);
+  const [coverPic, setCoverPic] = useState(null);
+
+  const [selectedImage, setSelectedImage] = useState(
+    "https://www.kindpng.com/picc/m/463-4636159_cloud-upload-icon-circle-hd-png-download.png"
+  );
+  const [selectedCover, setSelectedCover] = useState("");
+  function handleImageSelect(e) {
+    const file = e.target.files[0];
+    if (file) {
+      setProfilePic(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function handleCoverSelect(e) {
+    const file = e.target.files[0];
+    if (file) {
+      setCoverPic(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedCover(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
   return (
     <div className="h-screen overflow-y-auto bg-[#121212] text-white flex justify-center items-center">
       <div className="mx-auto my-8 flex w-full max-w-sm flex-col px-4">
@@ -57,6 +91,61 @@ function Signup() {
           />
         </div>
         <form onSubmit={handleSubmit(createAccount)} className="flex flex-col">
+          <div
+            className="w-full rounded-lg  bg-gray-300 text-purple-700  bg-cover bg-center bg-no-repeat items-center"
+            style={{
+              backgroundImage: `url(${selectedCover})`,
+            }}
+          >
+            <div
+              className={`mx-auto mt-6 flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-cover bg-center bg-no-repeat border-2 border-purple-700 `}
+              style={{ backgroundImage: `url(${selectedImage})` }}
+            >
+              <div className="bg-white/90 flex justify-center items-center rounded-full w-7 h-7 text-center ml-28 mt-[106px]">
+                <input
+                  type="file"
+                  style={{ display: "none" }}
+                  id="profileImg"
+                  accept="image/png, image/jpg, image/jpeg, image/gif"
+                  {...register("profileImg", { required: true })}
+                  onChange={(e) => {
+                    handleImageSelect(e);
+                  }}
+                />
+
+                <label
+                  htmlFor="profileImg"
+                  className="
+                   cursor-pointer"
+                >
+                  <MdOutlineCloudUpload />
+                </label>
+              </div>
+            </div>
+            <div className="flex justify-end">
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="coverphoto"
+                accept="image/png, image/jpg, image/jpeg, image/gif"
+                {...register("coverphoto", { required: false })}
+                onChange={(e) => {
+                  handleCoverSelect(e);
+                }}
+              />
+
+              <div className="bg-white/90 text-purple-700 flex items-center gap-1 rounded-tl-md px-2 text-center font-semibold">
+                <label
+                  htmlFor="coverphoto"
+                  className="inline-flex items-center gap-1 cursor-pointer"
+                >
+                  Cover
+                  <MdOutlineCloudUpload />
+                </label>
+              </div>
+            </div>
+          </div>
+
           <Input
             label={"Full Name*"}
             type="text"
